202c07e6e9b1224b48815f47c9c8c0b43ae8c93f
Hank Wu <hank880907@gmail.com>
2026-05-26 19:57:46 +0000

    VIB-8 Add AppShell three-column layout

---
+++ apps/web/src/routes/AppShell.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router'
+import { Sidebar } from '../components/Sidebar'
+import { RailActivity } from '../components/RailActivity'
+import { RailBackend } from '../components/RailBackend'
+
+export function AppShell() {
+  return (
+    <div className="flex h-screen w-screen overflow-hidden">
+      <Sidebar />
+      <main className="flex flex-1 flex-col overflow-hidden">
+        <Outlet />
+      </main>
+      <aside className="flex w-[240px] flex-col gap-4 border-l border-border bg-surface-rail p-4">
+        <RailActivity />
+        <div className="flex-1" />
+        <RailBackend />
+      </aside>
+    </div>
+  )
+}
